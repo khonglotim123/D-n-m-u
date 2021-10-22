2176f02e03032660c4164cbeab72f5ba9500e535
khonglotim123 <xagaph13499@fpt.edu.vn>
2021-10-22 08:34:00 +0700

    sfg

--- 3_GUI_PresetationLayer/FrmQuenMatKhau.cs
+++ 3_GUI_PresetationLayer/FrmQuenMatKhau.cs
@@ -60,9 +60,26 @@ namespace _3_GUI_PresetationLayer
             _nhanVien.So = 0;
             MessageBox.Show(_qlNhanVien.ChagePassWord(_nhanVien),"Thông báo");
             SendMail(_nhanVien.Email, _nhanVien.MatKhau);
+            _nhanVien.MatKhau = 
 
         }
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
 
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             FrmDangNhap frmDangNhap = new FrmDangNhap();
